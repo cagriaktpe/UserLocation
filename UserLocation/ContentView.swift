a9db743af059c2a932554bad35ed2c29c58447fc
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2024-03-08 11:22:20 +0300

    Initial Commit

---
+++ UserLocation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UserLocation
+//
+//  Created by Samet Çağrı Aktepe on 8.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UserLocation/UserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserLocationApp.swift
+//  UserLocation
+//
+//  Created by Samet Çağrı Aktepe on 8.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct UserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
